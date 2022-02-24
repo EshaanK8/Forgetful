67375a079f9c6aeec78ef3c51c7a6a584764aa1b
Eshaan Kunchur <64331800+EshaanK8@users.noreply.github.com>
2022-02-23 19:49:41 -0500

    Initial Commit

---
+++ ForgotApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForgotApp
+//
+//  Created by Eshaan Kunchur on 2022-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ForgotApp/ForgotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgotAppApp.swift
+//  ForgotApp
+//
+//  Created by Eshaan Kunchur on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct ForgotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
